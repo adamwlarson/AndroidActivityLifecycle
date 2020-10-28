61d0db6c1e52c6e010132433ef2e9f512fdb2c51
Adam Larson <maddogawl@gmail.com>
2020-10-27 20:02:46 -0400

    update androidx imports

--- app/src/main/java/edu/uwgb/class11_stack/MainActivity.java
+++ app/src/main/java/edu/uwgb/class11_stack/MainActivity.java
@@ -1,9 +1,10 @@
 package edu.uwgb.class11_stack;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
